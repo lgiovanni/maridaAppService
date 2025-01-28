2eac51e4e8317480abfaff369e1196c0fe8f72d6
Giovanni Lancianese <glancianese@civitatis.com>
2025-01-28 22:05:12 +0100

    add new-relic and vercel

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+'use strict';
+
+exports.config = {
+  app_name: ['MaridaApp'],
+  license_key: 'eu01xx14ccaa91ffd663fad169fe29d2FFFFNRAL',
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+};
